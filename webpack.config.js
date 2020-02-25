5a4e071e9ff33d780489419e3acc14808180e488
AnriiAndrushko <andry.i.andrushko@gmail.com>
2020-02-25 21:19:40 +0200

    All done!

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/test.js',
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  
+};
